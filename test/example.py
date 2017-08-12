5a81670bc134b22562dc474c1787efbc84f2aaf2
Swan <swan@reborn.com>
2017-08-12 17:43:18 -0400

    Remove Test .osu files

--- test/example.py
+++ test/example.py
@@ -4,12 +4,12 @@ sys.path.append("..")
 from deflate import fix_star_rating
 
 # The beatmap that you are checking and fixing inflated patterns for
-beatmap_path = "./gravity.osu"
+beatmap_path = "./necro.osu"
 
 # The new difficulty name (Version) of the beatmap
 new_difficulty_name = "star rating fix"
 
 # The output file path of the beatmap
-output_path = "./gravity_fixed.osu"
+output_path = "./necro_fixed.osu"
 
 fix_star_rating(beatmap_path, new_difficulty_name, output_path)
