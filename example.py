9737ddbe0073ee42b7d4d0f4e30701e8570efcf7
Swan <swan@reborn.com>
2017-08-12 15:56:52 -0400

    Add: More Test Files

--- example.py
+++ example.py
@@ -1,12 +1,12 @@
 from deflate import fix_star_rating
 
 # The beatmap that you are checking and fixing inflated patterns for
-beatmap_path = "./test/Chip Skylark - Shiny Teeth (Of Mass M Remix) (Swan) [Vibro Teeth].osu"
+beatmap_path = "./test/gravity.osu"
 
 # The new difficulty name (Version) of the beatmap
-new_difficulty_name = "fixed"
+new_difficulty_name = "star rating fix"
 
 # The output file path of the beatmap
-output_path = "./test/Chip Skylark - Shiny Teeth (Of Mass M Remix) (Swan) [fixed].osu"
+output_path = "./test/gravity_fixed.osu"
 
 fix_star_rating(beatmap_path, new_difficulty_name, output_path)
