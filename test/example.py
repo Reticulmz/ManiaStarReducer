3768d2f39de6bd46111bdeead128cd15053e467a
Swan <swan@reborn.com>
2017-08-12 15:58:24 -0400

    Move: Example usage script to /test

--- test/example.py
+++ test/example.py
@@ -1,12 +1,12 @@
 from deflate import fix_star_rating
 
 # The beatmap that you are checking and fixing inflated patterns for
-beatmap_path = "./test/gravity.osu"
+beatmap_path = "./gravity.osu"
 
 # The new difficulty name (Version) of the beatmap
 new_difficulty_name = "star rating fix"
 
 # The output file path of the beatmap
-output_path = "./test/gravity_fixed.osu"
+output_path = "./gravity_fixed.osu"
 
 fix_star_rating(beatmap_path, new_difficulty_name, output_path)
